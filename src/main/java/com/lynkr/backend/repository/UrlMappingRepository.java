4a723e630593c0f128880ee152e0e6c90e00bd1c
Lokesh Soni <verrmaji001@gmail.com>
2026-08-12 10:59:09 +0530

    Fix: Add missing User import in UrlMappingRepository

--- src/main/java/com/lynkr/backend/repository/UrlMappingRepository.java
+++ src/main/java/com/lynkr/backend/repository/UrlMappingRepository.java
@@ -1,6 +1,7 @@
 package com.lynkr.backend.repository;
 
 import com.lynkr.backend.model.UrlMapping;
+import com.lynkr.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
